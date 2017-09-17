ac1480306e65166294e6c8af9bbca555d052f694
Samuel Lee <sleeinsuk@gmail.com>
2017-09-17 16:31:07 -0400

    Added flashing lonzo. Partner coding with jeff

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -3,14 +3,42 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state = { showImage: true };
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showImage: !previousState.showImage };
+      })
+    }, 300)
+  };
+
+  renderImage() {
     const pic = {
       uri: "http://content.draftexpress.com/upload/player/large/LonzoBall.jpg"
     };
+    if (this.state.showImage) {
+      return <Image source={pic} style={[{width: 193, height: 110}]}/>
+    }
+  }
 
+  render() {
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={[{alignItems: 'center'}, styles.container]}>
+        <Greeting name='Jeff'/>
+        <Greeting name='Sam'/>
+        { this.renderImage() }
+      </View>
     );
+  };
+}
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    )
   }
 }
 
